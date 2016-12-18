059307c49c7e45a2bfc24265e355b3cd22dfff62
Xinyuan Wu <xinyuanwu9@gmail.com>
2016-12-18 03:28:06 -0500

    update

--- Flask/routes.py
+++ Flask/routes.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template, request, jsonify, url_for
-
+from model.CB.content_based_recommendation import *
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template("index.html")
-
 @app.route("/index.html")
 def index():
     return render_template("index.html")
@@ -15,9 +13,28 @@ def index():
 def viewbeers():
     return render_template("viewbeers.html")
 
-@app.route("/content.html")
+@app.route("/content.html", methods=['GET', 'POST'])
 def content():
-    return render_template("content.html")
+    with open("./model/CB/beer_list.p", "rb") as bf:
+        beer_list = pickle.load(bf)
+    with open("./model/CB/index.p", "rb") as idxf:
+        index = pickle.load(idxf)
+
+    # beer_inp = None
+
+    # if form.validate():
+    print "456"
+    if request.method == "POST" and "beer_inp" in request.form:
+        beer_inp = request.form["beer_inp"]
+
+        cb_rec = get_similar_beers(beer_inp, beer_list, index)
+        print str(cb_rec)
+
+        return render_template("content.html", cb_rec=cb_rec)
+
+    else:
+        # print "123"
+        return render_template("content.html")
 
 @app.route("/collab.html")
 def collab():
